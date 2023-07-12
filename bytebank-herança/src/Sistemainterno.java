918f16e143e68ae2c7a11c1baa4c0f5cb17324a3
EliseuZeu <eliseu.souza@mail.usf.edu.br>
2023-07-12 14:16:12 -0300

    Crianco uma classe Sistema interno

---
+++ bytebank-herança/src/Sistemainterno.java
@@ -0,0 +1,13 @@
+
+public class Sistemainterno {
+    private int senha = 2222;
+
+    public void autentica(Gerente g) {
+        boolean autenticou = g.autentica(this.senha);
+        if (autenticou) {
+            System.out.println("Pode entra no sistema");
+        } else {
+            System.out.println("Nao pode entra no sistema");
+        }
+    }
+}
